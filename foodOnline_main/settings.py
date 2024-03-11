fe50f14fdcb13214e5b81a45c3c30cfddef3bb22
Raj Patel <rajpatel@192.168.0.106>
2024-03-11 21:04:34 +0530

    homepage setup , postgres configuration and .env-sample

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vhi7kud--i7p$p9uh@!m-ormlmrd=@2ofg^_rl#ri1n#sig4db"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = "foodOnline_main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['foodOnline_main/templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -74,10 +75,20 @@ WSGI_APPLICATION = "foodOnline_main.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+     
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": config('DB_NAME'),
+    "USER": config('DB_USER'),
+    "PASSWORD": config('DB_PASSWORD'),
+    "HOST": config('DB_HOST'),
+}
+    
+
 }
 
 
@@ -115,7 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'foodOnline_main' / 'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- foodOnline_main/views.py
+++ foodOnline_main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#this file is just module
+#this file is just module 
+
 #function for home page
 def home(request):
-    return HttpResponse("hello my world")
+    return render(request , 'home.html')
